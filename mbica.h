a1967256ace1f017ef4411f5ce7716523c82e9ae
Pawel Zubrycki <paw.zubr@gmail.com>
2012-01-09 01:21:40 +0100

    Added some placeholder classes.

--- mbica.h
+++ mbica.h
@@ -1,11 +1,48 @@
 #ifndef MBICA_H
 #define MBICA_H
 
+#include<armadillo>
+
 namespace mbica {
 
     class Mbica {
     public:
         Mbica();
+    };
+
+    class Pow3 {
+    public:
+        static arma::mat G(arma::mat X) {
+            return arma::pow(X, 3);
+        }
+
+        static arma::mat dG(arma::mat X) {
+            return 3*arma::pow(X, 2);
+        }
+    };
+
+    class PCA {
+    public:
+        static arma::mat process(arma::mat X) {
+            // Placeholder to count PCA
+            return arma::mat();
+        }
+    };
+
+    class Whitenig {
+        static arma::mat process(arma::mat X) {
+            // Placeholder to count PCA and whitening
+            return arma::mat();
+        }
+
+    };
+
+    template<int mu = 1, class Preprocess = Whitening, class UsedFunc = Pow3>
+    class FastICA {
+    public:
+        arma::mat count(arma::mat X) {
+
+        }
     };
 }
 #endif // MBICA_H
