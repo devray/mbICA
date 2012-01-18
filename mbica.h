febb56a8962a120805f52f7dd70040f44b9b9f22
Stanisław Janikowski <S.Janikowski@itl.waw.pl>
2012-01-18 19:19:27 +0100

    Uwagi do PCA, implementacja ortogonalizacji

--- mbica.h
+++ mbica.h
@@ -39,17 +39,20 @@ namespace mbica {
 
             eig_sym(D, E, C);
 
-            int stopIx = D.n_elem, startIx=0;
+            unsigned stopIx = D.n_elem, startIx=0;
             for(; stopIx > 0 && D[stopIx-1] == 0; --stopIx);
-            for(; startIx < D.n_elem && D[startIx] == 0; ++startIx);
+            // Wystarczy sprawdzić od 0 do stopIx, bo w anomalnym przypadku jak wszystkie są 0 to skończymy bez sensu z startIx=D.n_elem i stopIx=0
+            for(; startIx < stopIx && D[startIx] == 0; ++startIx);
 
             if(stopIx != D.n_elem || startIx != 0) {
                 D = D.subvec(startIx, stopIx-1);
                 // TODO: trzeba sprawdzic, czy eigenvectory w wierszach, czy kolumnach
+                // Z tego, co ja rozumiem, powinny być w kolumnach
                 E = E.cols(startIx, stopIx-1);
             }
 
             // TODO: sprawdzic, czy nie potrzebna jest transpozycja E - wszak transponujemy na poczatku X
+            // Od transpozycji tutaj zależałoby tylko czy eigenvectory w wyniku będą wierszach, czy kolumnach
         }
     };
 
@@ -64,6 +67,25 @@ namespace mbica {
             dWh = E * sqrtD;
         }
     };
+
+    mat orth(const mat& A){
+        mat U, V;
+        vec s;
+        svd(U,s,V,A);
+
+        if(A.n_rows > A.n_cols){
+            U = U.cols(0, A.n_cols-1);
+            s = s.subvec(0, A.n_cols-1);
+        }
+        double tolerance = std::max(A.n_rows,A.n_cols) * math::eps() * max(s);
+        int r;
+        for(int i=0; i < s.n_elem; ++i)
+            if (s[i] > tolerance)
+                ++r;
+
+        return U.cols(0,r-1);
+
+    }
 
     template<int mu = 1, class UsedFunc = Pow3>
     class FastICA {
@@ -96,7 +118,7 @@ namespace mbica {
             // B mozemy dac jako zgadniete, np, zeby znalezc wiecej IC
             // B = whiteningMatrix * guess;
             // orth z octave - musimy coś wymyslić
-            mat B = arma::orth(randu<mat>(X.n_rows, nIC) - 0.5),
+            mat B = orth(randu<mat>(X.n_rows, nIC) - 0.5),
                 B_old = arma::zeros<mat>(X.n_rows, nIC);
             int max_iterations = 1000, i;
 
