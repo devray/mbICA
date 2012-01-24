6d121591732bbbdbbd15eeb850893751a1f480c1
Pawel Zubrycki <paw.zubr@gmail.com>
2012-01-24 03:57:09 +0100

    Added octave function to plot icas

---
+++ plot_icas.m
@@ -0,0 +1,11 @@
+function plot_icas ( x, prefix = "result_")
+	nIC = min(size(x))
+	nSamples = max(size(x))
+
+	for i=1:nIC
+		plot((1:nSamples)/128, x(:, i))
+		xlim([0 nSamples/128])
+		print(strcat(prefix, num2str(i), ".png"), "-dpng")
+	end
+end
+
