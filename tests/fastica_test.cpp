8bef39cb4d7b7b8840600a52f943adb274a70793
Pawel Zubrycki <paw.zubr@gmail.com>
2012-01-25 00:07:50 +0100

    Little modified fastica test

--- tests/fastica_test.cpp
+++ tests/fastica_test.cpp
@@ -27,16 +27,17 @@ BOOST_AUTO_TEST_CASE(unmixing)
             mbica::FastICA<mbica::nonlinearities::Gauss<1, 1>, mbica::WithStabilization>()(X);
 
     cout << "Real A = \n" << (A >= 0.5)  << endl;
+    // Ugly way of getting found mixing matrix to former mixing matrix.
     cout << "Found A = \n" << (abs(sep2.getA()) >= 0.2) << endl;
-    // K is shufled eye matrix
+    // K is now a shufled eye matrix
     mat K = inv(A) * (abs(sep2.getA()) >= 0.2);
 
+    // K should be shuffled eye if
     BOOST_CHECK_SMALL(mat(abs(sep2.getA() * sep2.getW() - eye(3,3))).max(), 0.01);
 
     //Checking if matrixes are similar
     BOOST_WARN_SMALL(sum(sum(K % K)) - 3.0 , 0.00001);
     BOOST_WARN_SMALL(sum(sum(K)) - 3.0 , 0.00001);
-
 }
 
 BOOST_AUTO_TEST_SUITE_END()
