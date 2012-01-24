9953feaef5a312368786af98a687e1a4f7edcc08
Pawel Zubrycki <paw.zubr@gmail.com>
2012-01-24 17:49:25 +0100

    Added simple test for ICASeparator

--- tests/icaseparator_test.h
+++ tests/icaseparator_test.h
@@ -1,2 +1,19 @@
 #include "icaseparator.h"
 
+BOOST_AUTO_TEST_CASE(separator_multiplying)
+{
+    mat A = 2 * eye(3, 3);
+
+    mbica::ICASeparator sep(A, A);
+    mat B, C, D;
+    D << 1 << 2 << 3 << 4 << endr;
+    B = repmat(D, 3, 1);
+    C << 2 << 4 << 6 << 8 << endr;
+    C = repmat(C, 3, 1);
+
+    // Checking if sep(B) == C
+    BOOST_CHECK_SMALL(mat(abs(sep(B) - C)).max(), 0.000001);
+
+    // should throw because of bad D size
+    BOOST_CHECK_THROW(sep(D), std::logic_error);
+}
